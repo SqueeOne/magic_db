57209219a5df7d17c059e967c89542f5209b756c
SqueeOnE <selih.design@gmail.com>
2024-08-27 10:19:00 +0200

    Added seaarch bar component

--- magicdb/app/page.tsx
+++ magicdb/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchBar from '@/components/SearchBar'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,18 +7,8 @@ export default function Home() {
       <div className="w-full h-1/3 bg-black flex justify-center items-center">
         <h1 className="text-4xl font-bold text-white/90">MagicDB</h1>
       </div>
-      <div className="w-full h-1/3 flex justify-center items-center">
-        <form className="max-w-md mx-auto">
-          <label
-            htmlFor="base-search"
-            className="mb-2 text-sm font-medium text-grey-900 sr-only dark:text-white"
-          >
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
-            </div>
-            Search for users, cards, decks...
-          </label>
-        </form>
+      <div className="w-full h-1/3 flex items-center justify-center">
+        <SearchBar searchTerm={''} />
       </div>
     </main>
   )
